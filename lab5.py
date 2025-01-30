6b5b946044443a040289ee09f0351c28c77a8513
danielleG332 <dgarci332@calstatela.edu>
2025-01-30 13:57:27 -0800

    Add files via upload

---
+++ lab5.py
@@ -0,0 +1,14 @@
+Python 3.11.0 (v3.11.0:deaf509e8f, Oct 24 2022, 14:43:23) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #ask to enter input
+... #read input and store it
+... #check if number inputted is divisible by 2
+... #print if even(divisible) or odd(not divisible)
+... 
+... 
+... num=int(input("please enter a number to see if its even or odd: "))
+... 
+... if num %2==0:
+...     print(num, "is even")
+...     
+... else:
